db75cc19e92969cc846e35e83a6ae27ec1917d20
Samarth Dharpure <139703678+SamarthDharpure@users.noreply.github.com>
2025-06-14 21:55:23 +0530

    Update 2. Greeting.java

--- CHAPTER-01 (Introduction)/2. Greeting.java
+++ CHAPTER-01 (Introduction)/2. Greeting.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+class Greeting {
     public static void main(String[] args) {
           System.out.println("Hi, my name is Samarth Dharpure");
           System.out.println("How are you ?");                      
