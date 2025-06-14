7d22b2f7567a1e7ebdf701708ba692fa078b0fe9
Samarth Dharpure <139703678+SamarthDharpure@users.noreply.github.com>
2025-06-14 22:55:31 +0530

    Delete CHAPTER-01 (Introduction) directory

--- CHAPTER-01 (Introduction)/2. Greeting.java
+++
@@ -1,6 +0,0 @@
-class Greeting {
-    public static void main(String[] args) {
-          System.out.println("Hi, my name is Samarth Dharpure");
-          System.out.println("How are you ?");                      
-    }
-}
